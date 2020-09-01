73d4f02fea0b50c96fc94205e562c70c07e6dd51
rojung <roland.jung@aau.at>
2020-09-01 13:17:01 +0200

    [TASK] trajectory::PlotLineStyle() added

--- TrajectoryPlotConfig.py
+++ TrajectoryPlotConfig.py
@@ -1,6 +1,27 @@
 from trajectory.TrajectoryPlotTypes import TrajectoryPlotTypes
 
 
+# https://matplotlib.org/2.1.2/api/_as_gen/matplotlib.pyplot.plot.html
+class PlotLineStyle():
+    linecolor = 'r'  # b,g,r,c,m,y,k,w
+    linewidth = 1
+    linestyle = '--'  # '-', '--', '-.', ':'
+    marker = '.'  # '.', ',', 'o', 'v', '^', '<', '>', '1..4', 's', 'p'
+    markersize = 12
+    markerfacecolor = 'blue'
+    label = 'x'
+
+    def __init__(self, linecolor='r', linewidth=1, linestyle='--', marker='o', markersize=12,
+                 markerfacecolor='blue', label='x'):
+        self.linecolor = linecolor
+        self.linewidth = linewidth
+        self.linestyle = linestyle
+        self.marker = marker
+        self.markersize = markersize
+        self.markerfacecolor = markerfacecolor
+        self.label = label
+
+
 class TrajectoryPlotConfig():
     white_list = []
     num_points = []
