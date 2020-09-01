6f5fa91be3dab44123698dfb4fde34ae729f938d
rojung <roland.jung@aau.at>
2020-09-01 13:18:06 +0200

    [TASK] TrajectoryPlotter: using PlotLineStyle, plot_pose_err w. plotter_gt

--- TrajectoryPlotter.py
+++ TrajectoryPlotter.py
@@ -6,7 +6,7 @@ from mpl_toolkits.mplot3d import Axes3D  # <--- This is important for 3d plottin
 from csv2dataframe.TUMCSV2DataFrame import TUMCSV2DataFrame
 from trajectory.Trajectory import Trajectory
 from trajectory.TrajectoryEstimated import TrajectoryEstimated
-from trajectory.TrajectoryPlotConfig import TrajectoryPlotConfig
+from trajectory.TrajectoryPlotConfig import TrajectoryPlotConfig, PlotLineStyle
 from trajectory.TrajectoryPlotTypes import TrajectoryPlotTypes
 from trajectory.pyplot_utils import set_axes_equal
 
@@ -100,17 +100,19 @@ class TrajectoryPlotter:
     @staticmethod
     def ax_plot_n_dim(ax, x_linespace, values,
                       colors=['r', 'g', 'b'],
-                      labels=['x', 'y', 'z']):
+                      labels=['x', 'y', 'z'], ls=PlotLineStyle()):
         assert len(colors) == len(labels)
         if len(colors) > 1:
             assert len(colors) == values.shape[1]
             for i in range(len(colors)):
                 ax.plot(x_linespace, values[:, i],
-                        colors[i] + '-', label=labels[i])
+                        color=colors[i], label=labels[i], linestyle=ls.linestyle, linewidth=ls.linewidth,
+                        marker=ls.marker)
         else:
-            ax.plot(x_linespace, values, colors[0] + '-', label=labels[0])
+            ax.plot(x_linespace, values, color=colors[0], label=labels[0], linestyle=ls.linestyle,
+                    linewidth=ls.linewidth)
 
-    def ax_plot_pos(self, ax, cfg):
+    def ax_plot_pos(self, ax, cfg, colors=['r', 'g', 'b'], labels=['x', 'y', 'z'], ls=PlotLineStyle()):
         ts, xs, ys, zs, dist_vec = self.get_pos_data(cfg)
 
         if cfg.plot_type == TrajectoryPlotTypes.plot_2D_over_dist:
@@ -123,13 +125,13 @@ class TrajectoryPlotter:
         ax.set_ylabel('position [m]')
 
         if len(xs):
-            TrajectoryPlotter.ax_plot_n_dim(ax, linespace, xs, colors=['r'], labels=['x'])
+            TrajectoryPlotter.ax_plot_n_dim(ax, linespace, xs, colors=[colors[0]], labels=[labels[0]], ls=ls)
         if len(ys):
-            TrajectoryPlotter.ax_plot_n_dim(ax, linespace, ys, colors=['g'], labels=['y'])
+            TrajectoryPlotter.ax_plot_n_dim(ax, linespace, ys, colors=[colors[1]], labels=[labels[1]], ls=ls)
         if len(zs):
-            TrajectoryPlotter.ax_plot_n_dim(ax, linespace, zs, colors=['b'], labels=['z'])
+            TrajectoryPlotter.ax_plot_n_dim(ax, linespace, zs, colors=[colors[2]], labels=[labels[2]], ls=ls)
 
-    def ax_plot_rpy(self, ax, cfg):
+    def ax_plot_rpy(self, ax, cfg, colors=['r', 'g', 'b'], labels=['x', 'y', 'z'], ls=PlotLineStyle()):
         ts, xs, ys, zs, dist_vec = self.get_rpy_data(cfg)
 
         if cfg.plot_type == TrajectoryPlotTypes.plot_2D_over_dist:
@@ -146,13 +148,14 @@ class TrajectoryPlotter:
             ax.set_ylabel('rotation [deg]')
 
         if len(xs):
-            TrajectoryPlotter.ax_plot_n_dim(ax, linespace, xs, colors=['r'], labels=['x'])
+            TrajectoryPlotter.ax_plot_n_dim(ax, linespace, xs, colors=[colors[0]], labels=[labels[0]], ls=ls)
         if len(ys):
-            TrajectoryPlotter.ax_plot_n_dim(ax, linespace, ys, colors=['g'], labels=['y'])
+            TrajectoryPlotter.ax_plot_n_dim(ax, linespace, ys, colors=[colors[1]], labels=[labels[1]], ls=ls)
         if len(zs):
-            TrajectoryPlotter.ax_plot_n_dim(ax, linespace, zs, colors=['b'], labels=['z'])
+            TrajectoryPlotter.ax_plot_n_dim(ax, linespace, zs, colors=[colors[2]], labels=[labels[2]], ls=ls)
 
-    def ax_plot_q(self, ax, cfg):
+    def ax_plot_q(self, ax, cfg, colors=['r', 'g', 'b', 'k'],
+                  labels=['qx', 'qy', 'qz', 'qw'], ls=PlotLineStyle()):
         q_vec = self.traj_obj.q_vec
         if cfg.plot_type == TrajectoryPlotTypes.plot_2D_over_dist:
             x_linespace = self.traj_obj.get_accumulated_distances()
@@ -163,8 +166,8 @@ class TrajectoryPlotter:
             ax.set_xlabel('rel. time [sec]')
         ax.set_ylabel('quaternion')
 
-        TrajectoryPlotter.ax_plot_n_dim(ax, x_linespace, q_vec, colors=['r', 'g', 'b', 'k'],
-                                        labels=['qx', 'qy', 'qz', 'qw'])
+        TrajectoryPlotter.ax_plot_n_dim(ax, x_linespace, q_vec, colors=colors,
+                                        labels=labels)
 
     def ax_plot_pos_3D(self, ax, cfg=None, label="trajectory"):
         if cfg is None:
@@ -257,7 +260,7 @@ class TrajectoryPlotter:
         return fig, ax
 
     @staticmethod
-    def plot_pose_err(plotter_est, plotter_err, fig=None, cfg=None, angles=False):
+    def plot_pose_err(plotter_est, plotter_err, fig=None, cfg=None, angles=False, plotter_gt=None):
         if cfg is None:
             cfg = plotter_est.config
 
@@ -270,12 +273,18 @@ class TrajectoryPlotter:
         ax4 = fig.add_subplot(224)
 
         plotter_est.ax_plot_pos(ax=ax1, cfg=cfg)
+        if plotter_gt:
+            plotter_gt.ax_plot_pos(ax=ax1, cfg=cfg, colors=['k', 'k', 'k'], labels=['gt_x', 'gt_y', 'gt_z'],
+                                   ls=PlotLineStyle(linestyle='-.', linewidth=0.5))
         plotter_err.ax_plot_pos(ax=ax2, cfg=cfg)
         ax1.set_ylabel('position est [m]')
         ax2.set_ylabel('position err [m]')
 
         if angles:
             plotter_est.ax_plot_rpy(ax=ax3, cfg=cfg)
+            if plotter_gt:
+                plotter_gt.ax_plot_rpy(ax=ax3, cfg=cfg, colors=['k', 'k', 'k'],
+                                       ls=PlotLineStyle(linestyle='-.', linewidth=0.5))
             plotter_err.ax_plot_rpy(ax=ax4, cfg=cfg)
             if cfg.radians:
                 ax3.set_ylabel('rotation est [rad]')
@@ -285,6 +294,9 @@ class TrajectoryPlotter:
                 ax4.set_ylabel('rotation err [deg]')
         else:
             plotter_est.ax_plot_q(ax=ax3, cfg=cfg)
+            if plotter_gt:
+                plotter_gt.ax_plot_q(ax=ax3, cfg=cfg, colors=['k', 'k', 'k', 'k'],
+                                     ls=PlotLineStyle(linestyle='-.', linewidth=0.5))
             plotter_err.ax_plot_q(ax=ax4, cfg=cfg)
             ax4.set_ylabel('quaternion err')
 
@@ -362,7 +374,8 @@ class TrajectoryPlotter_Test(unittest.TestCase):
 
     def test_plot_estimated_traj(self):
         plotter = TrajectoryPlotter(traj_obj=self.load_trajectory3_from_CSV())
-        plotter.plot_pose()
+        plotter.plot_pose(angles=True, cfg=TrajectoryPlotConfig(radians=False,
+                                                                plot_type=TrajectoryPlotTypes.plot_2D_over_t))
         plotter.plot_3D()
         print('done')
 
