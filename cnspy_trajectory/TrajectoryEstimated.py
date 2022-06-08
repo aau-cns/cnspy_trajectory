3d1807f144d655abffb0f76d1a51eebc9bd94a05
rojung <roland.jung@aau.at>
2022-06-09 00:01:58 +0200

    [FEAT] TrajectoryEstimated: member est. error type + error repr. type

--- cnspy_trajectory/TrajectoryEstimated.py
+++ cnspy_trajectory/TrajectoryEstimated.py
@@ -18,10 +18,16 @@
 #
 ########################################################################################################################
 import os
+
+import pandas
+from enum import Enum
 from cnspy_trajectory.Trajectory import Trajectory
 from cnspy_csv2dataframe.PoseWithCov2DataFrame import PoseWithCov2DataFrame
 from cnspy_csv2dataframe.CSV2DataFrame import CSV2DataFrame
 from cnspy_spatial_csv_formats.CSVFormatPose import CSVFormatPose
+from cnspy_trajectory.EstimationErrorType import EstimationErrorType
+from cnspy_trajectory.RotationErrorRepresentationType import RotationErrorRepresentationType
+
 
 
 class TrajectoryEstimated(Trajectory):
@@ -29,17 +35,41 @@ class TrajectoryEstimated(Trajectory):
     # The with upper triangular elements are vectorized: 'pxx', 'pxy', 'pxz', 'pyy', 'pyz', 'pzz'
     Sigma_p_vec = None
 
-    # small angle `theta` uncertainty: R = ( eye(3) + slew(theta)), a 3x3 covariance matrix.
-    # The with upper triangular elements are vectorized: 'qrr', 'qrp', 'qry', 'qpp', 'qpy', 'qyy'
+    # small angle `theta` uncertainty: R = ( eye(3) + skew(theta_R)), a 3x3 covariance matrix.
+    # The with upper triangular elements are vectorized in radiant: 'qrr', 'qrp', 'qry', 'qpp', 'qpy', 'qyy'
     Sigma_q_vec = None
 
-    def __init__(self, t_vec=None, p_vec=None, q_vec=None, Sigma_p_vec=None, Sigma_q_vec=None, df=None):
+    # specifies if the position uncertainty is defined w.r.t. the global frame (W) or the local/body frame (B)
+    # p_WB_in_W/B_err \sim N(0, Sigma_p)
+    # - True: p_WB_in_W_true = p_WB_in_W_est + p_WB_in_W_err;
+    # - False: p_WB_in_W_true = p_WB_in_W_est + R(q_WB_est)*p_WB_in_B_err
+    #global_p_uncertainty = True
+    # specifies if the orientation uncertainty is defined w.r.t. the global frame (W) or the local/body frame (B)
+    # q_WB_err \sim N(0, Sigma_q)
+    # - True = R(q_WB_true) = R(q_WB_err)*R(q_WB_est)
+    # - False: R(q_WB_true) = R(q_WB_est)*R(q_WB_err)
+    #global_q_uncertainty = False
+
+    # specifies the uncertainty of the rotation (Sigma_q_vec)
+    rot_err_representation = RotationErrorRepresentationType.R_small_theta
+    # specifies global or local definitions of the uncertainty (Sigma_p/q_vec)
+    estimation_error_type = EstimationErrorType.type5
+
+    def __init__(self, t_vec=None, p_vec=None, q_vec=None, Sigma_p_vec=None, Sigma_q_vec=None, df=None,
+                 rot_err_representation=RotationErrorRepresentationType.R_small_theta,
+                 estimation_error_type=EstimationErrorType.type1):
         Trajectory.__init__(self, t_vec=t_vec, p_vec=p_vec, q_vec=q_vec)
         self.Sigma_p_vec = Sigma_p_vec
         self.Sigma_q_vec = Sigma_q_vec
 
         if df is not None:
             self.load_from_DataFrame(df)
+
+        if rot_err_representation is not None:
+            self.rot_err_representation = rot_err_representation
+
+        if estimation_error_type is not None:
+            self.estimation_error_type = estimation_error_type
 
     def load_from_CSV(self, filename):
         if not os.path.isfile(filename):
@@ -51,8 +81,9 @@ class TrajectoryEstimated(Trajectory):
         return loader.data_loaded
 
     def load_from_DataFrame(self, df):
-        self.t_vec, self.p_vec, self.q_vec, self.Sigma_p_vec, self.Sigma_q_vec = PoseWithCov2DataFrame.DataFrame_to_TPQCov(
-            data_frame=df)
+        assert (isinstance(df, pandas.DataFrame))
+        self.t_vec, self.p_vec, self.q_vec, self.Sigma_p_vec, \
+            self.Sigma_q_vec = PoseWithCov2DataFrame.DataFrame_to_TPQCov(data_frame=df)
 
     def to_DataFrame(self):
         return PoseWithCov2DataFrame.TPQCov_to_DataFrame(self.t_vec, self.p_vec, self.q_vec, self.Sigma_p_vec,
