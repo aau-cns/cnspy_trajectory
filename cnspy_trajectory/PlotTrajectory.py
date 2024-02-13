7e09fa889c1e4a593e7a4f948cf2f7c3b2f2e1b0
rojung <roland.jung@aau.at>
2024-02-13 14:44:14 +0100

    [FEAT] PlotTrajectory::plot_multi() added

--- cnspy_trajectory/PlotTrajectory.py
+++ cnspy_trajectory/PlotTrajectory.py
@@ -28,6 +28,7 @@ from cnspy_trajectory import TrajectoryPlotTypes
 from cnspy_trajectory.Trajectory import Trajectory
 from cnspy_trajectory.TrajectoryPlotConfig import TrajectoryPlotConfig
 from cnspy_trajectory.TrajectoryPlotTypes import TrajectoryPlotTypes
+from cnspy_trajectory.TrajectoryPlotter import TrajectoryPlotter
 
 
 class PlotTrajectory:
@@ -35,13 +36,13 @@ class PlotTrajectory:
         pass
 
     @staticmethod
-    def plot(fn, result_dir="", verbose=False, plot_3D=True, plot_pose=True, show_plots=True):
+    def plot_single(fn, result_dir=None, verbose=False, plot_3D=True, plot_pose=True, show_plots=True, save_metrics=True):
         if not os.path.isfile(fn):
             print("PlotTrajectory: could not find file: %s" % fn)
             return False
 
         ## create result dir:
-        if result_dir == "":
+        if result_dir is None:
             folder = str(fn).replace(".csv", "")
         else:
             folder = result_dir
@@ -71,37 +72,77 @@ class PlotTrajectory:
                 cfg.save_fn=str(folder + '/pose_over_dist.png')
                 traj.plot_pose(cfg=cfg)
 
-            if verbose:
-                traj.print_statistics()
-            with open(folder + '/metrics.txt', 'w') as file:
-                traj.print_statistics(file=file)
+            if save_metrics:
+                if verbose:
+                    traj.print_statistics()
+                with open(folder + '/metrics.txt', 'w') as file:
+                    traj.print_statistics(file=file)
 
         else:
             print("* Trajectory was not loaded!")
             return False
         return True
 
+    @staticmethod
+    def plot_multi(fns, result_dir=None, verbose=False, show_plots=True):
+        folder = result_dir
+        traj_arr = []
+        for fn in fns:
+            if not os.path.isfile(fn):
+                print("* WARNING: could not find file: %s" % fn)
+                continue
+            ## create result dir:
+            if folder is None:
+                folder = str(fn).replace(".csv", "")
+                folder = os.path.abspath(folder)
+                try:  # else already exists
+                    [head, tail] = os.path.split(folder)
+                    folder = head
+                    os.makedirs(folder)
+                except:
+                    pass
+                if verbose:
+                    print("* result_dir: \t " + str(folder))
+
+            traj = Trajectory()
+            traj.load_from_CSV(fn=fn)
+            if not traj.is_empty():
+                traj_arr.append(traj)
+            else:
+                print("* WARNING trajectory is empty! fn=" + str(fn))
+
+        if traj_arr:
+            cfg = TrajectoryPlotConfig(show=show_plots, close_figure=True,
+                                       save_fn=str(folder + '/multiplot_3D.png'))
+            TrajectoryPlotter.multi_plot_3D(traj_arr, cfg=cfg)
+
+        return True
+
 
 if __name__ == "__main__":
-    # test3: python3 ROSbag2CSV.py --bagfile ../test/example.bag --topics /CS_200_MAV1/estimated_poseWithCov  /pose_sensor/pose --verbose --filename mav_PoseWithCov.csv sensor_PoseWithCov.csv --format PoseWithCov
-    # test4: python3 ROSbag2CSV.py --bagfile ./sample_data/empty_bag.bag --topics /uwb_trilateration/tagDistance_raw /pose_sensor/pose /fcu/current_pose --verbose  --filenames uwb /rasdf/body_pose imu_pose.csv
-    # test5: python3 ROSbag2CSV.py --bagfile ./sample_data/dummy.bag --topics /pose_est /pose_gt --verbose  --filenames est gt --format TUM
     parser = argparse.ArgumentParser(
         description='PlotTrajectory: tool to save plots and metrics of a trajectory')
-    parser.add_argument('--filename', help='csv filename', default="not specified")
+    parser.add_argument('--filename', help='csv filename', default=None)
+    parser.add_argument('--filenames', type=str, nargs='+',
+                        help='a list of files to be plotted in 3D', default=None)
     parser.add_argument('--result_dir', help='directory to store results]',
-                        default='')
+                        default=None)
     parser.add_argument('--verbose', action='store_true', default=False)
     parser.add_argument('--plot_3D', action='store_true', default=False)
     parser.add_argument('--plot_pose', action='store_true', default=False)
     parser.add_argument('--show_plots', action='store_true', default=False)
+    parser.add_argument('--save_metrics', action='store_true', default=False)
 
 
     tp_start = time.time()
     args = parser.parse_args()
-    if PlotTrajectory.plot(args.filename, result_dir=args.result_dir, verbose=args.verbose,
-                           plot_3D=args.plot_3D, plot_pose=args.plot_pose, show_plots=args.show_plots):
-        print(" ")
-        print("finished after [%s sec]\n" % str(time.time() - tp_start))
-    else:
-        print("failed! after [%s sec]\n" % str(time.time() - tp_start))
+
+    if args.filename is not None:
+        PlotTrajectory.plot_single(args.filename, result_dir=args.result_dir, verbose=args.verbose,
+                               plot_3D=args.plot_3D, plot_pose=args.plot_pose, show_plots=args.show_plots,
+                               save_metrics=args.save_metrics)
+
+    elif args.filenames is not None:
+        PlotTrajectory.plot_multi(args.filenames, result_dir=args.result_dir, show_plots=args.show_plots, verbose=args.verbose)
+
+    print("finished after [%s sec]\n" % str(time.time() - tp_start))
